455bd095d064231fb25adec567cfe3e1f12ab7ec
Aloysius Juan <juanfarrel0404@gmail.com>
2024-02-04 20:45:29 +0700

    Minor

--- src/components/sponsor/Sponsor.js
+++ src/components/sponsor/Sponsor.js
@@ -9,11 +9,8 @@ function Sponsor() {
             <h1 className='font-primary w-[60%] text-xl lg:text-5xl lg:w-[40%] '>SPONSORS & MEDIA PARTNER</h1>
             <div className='flex flex-col justify-center items-center gap-8 mt-12 mb-16 lg:text-3xl lg:mt-32'>
                 <h1 className='font-primary text-md'>SPONSORS</h1>
-                <p className='lg:mb-12'>GAONO SPONSOR. PROJECT ISENG</p>
                 <h1 className='font-primary text-md'>OFFICIAL PARTNERS</h1>
-                <p className='lg:mb-12'>GADUWE PARTNER. SEK JOMBLO.</p>
                 <h1 className='font-primary text-md'>MEDIA PARTNERS</h1>
-                <p className='lg:mb-12'>GITHUB KU DEWE. NDANG FOLLOW!!!</p>
             </div>
         </div>
     </>
